20e2b8470270cea6657dc8aad15547195bdea32e
Srinivas Mamillapalli <srimamilla79@gmail.com>
2025-06-17 16:56:33 -0400

    SECURITY: Update to new Supabase API keys

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     env: {
       NEXT_PUBLIC_SUPABASE_URL: 'https://jhcfuvblgcznhcrpfayb.supabase.co',
       NEXT_PUBLIC_SUPABASE_ANON_KEY:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoY2Z1dmJsZ2N6bmhjcnBmYXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMjkyNzYsImV4cCI6MjA2NDgwNTI3Nn0.FhnCoDle6sgiBg4GCvN53MKEF7ZWgtIQ_tZQeAjglDU',
+        'sb_publishable_EsVN4ztP-WWjk9EpBCk4qA_UnyECi7t',
     },
   }),
 
